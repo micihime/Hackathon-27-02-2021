b29870e084aebb4c25e8bd0ef9b94d8db737bc54
micihime <hroncovam@yahoo.com>
2021-02-27 11:36:18 +0100

    Get summary for period

--- HackathonAPI/TravelGreen/TravelGreen/Controllers/ValuesController.cs
+++ HackathonAPI/TravelGreen/TravelGreen/Controllers/ValuesController.cs
@@ -22,17 +22,17 @@ namespace TravelGreen.Controllers
             return dashboard;
         }
 
-        //// GET api/values
-        //public List<Summary> GetSummary(int period)
-        //{
-        //    //call method that computes summary from specified time period
-        //    var summaries = new List<Summary>()
-        //        {
-        //            new Summary { Transport = TransportType.Car, Minutes = 1500, FootprintSum = 169},
-        //            new Summary { Transport = TransportType.Bus, Minutes = 237, FootprintSum = 24}
-        //    };
-        //    return summaries;
-        //}
+        // GET api/values/5
+        public List<Summary> Get(int id)
+        {
+            //call method that computes summary from specified time period
+            var summaries = new List<Summary>()
+                    {
+                        new Summary { Transport = TransportType.Car, Minutes = 1500, FootprintSum = 169},
+                        new Summary { Transport = TransportType.Bus, Minutes = 237, FootprintSum = 24}
+                };
+            return summaries;
+        }
 
         // GET api/values/5
         [HttpPost]
