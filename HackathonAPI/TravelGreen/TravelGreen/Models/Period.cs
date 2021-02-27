d9c8863fb1034df254afa6475102bc8557871f86
mhreskov <miroslava.hreskova@t-systems.com>
2021-02-27 10:48:06 +0100

    Uninstalling swagger - some error

--- HackathonAPI/TravelGreen/TravelGreen/Models/Period.cs
+++ HackathonAPI/TravelGreen/TravelGreen/Models/Period.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace TravelGreen.Models
+﻿namespace TravelGreen.Models
 {
     public enum Period
     {
