8dd00eae9d9fce9d60cc842d94be8ae7b815941f
micihime <hroncovam@yahoo.com>
2021-02-27 11:57:35 +0100

    Adding transport controller

---
+++ HackathonAPI/TravelGreen/TravelGreen/Controllers/TransportController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using TravelGreen.Models;
+
+namespace TravelGreen.Controllers
+{
+    public class TransportController : ApiController
+    {
+        // GET api/values
+        public Dashboard GetDashboard()
+        {
+            var dashboard = new Dashboard()
+            {
+                AverageMonthlyFootprint = 100,
+                Summaries = new List<Summary>()
+                {
+                    new Summary { Transport = TransportType.Car, Minutes = 1500, FootprintSum = 169},
+                    new Summary { Transport = TransportType.Bus, Minutes = 237, FootprintSum = 24}
+                },
+                Insight = "Hlaska porovnanie oproti min mesiacu"
+            };
+            return dashboard;
+        }
+
+        // GET api/values/5
+        public List<Summary> Get(int id)
+        {
+            //call method that computes summary from specified time period
+            var summaries = new List<Summary>()
+                    {
+                        new Summary { Transport = TransportType.Car, Minutes = 1500, FootprintSum = 169},
+                        new Summary { Transport = TransportType.Bus, Minutes = 237, FootprintSum = 24}
+                };
+            return summaries;
+        }
+
+        // GET api/values/5
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] Entry entry)
+        {
+            if (entry == null)
+                return BadRequest("Entry null.");
+            else
+                //save
+                return Ok();
+        }
+    }
+}
--- HackathonAPI/TravelGreen/TravelGreen/Models/Entry.cs
+++ HackathonAPI/TravelGreen/TravelGreen/Models/Entry.cs
@@ -1,8 +1,11 @@
-﻿namespace TravelGreen.Models
+﻿using System;
+
+namespace TravelGreen.Models
 {
     public class Entry
     {
         //public long ID { get; set; }
+        public DateTime Date { get; set; }
 
         public TransportType Transport { get; set; }
 
